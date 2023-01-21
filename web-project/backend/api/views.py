6d117b8844c36f59c03bc3cdc235abbbe389179a
Przemysław Sałek <psalek1999@gmail.com>
2023-01-21 12:26:26 +0100

    reformat code

--- web-project/backend/api/views.py
+++ web-project/backend/api/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import TodoSerializer
+
 from .models import Todo
+from .serializers import TodoSerializer
+
 
 # Create your views here.
 
