7d3865cf10576d2d04758c17945622919681d007
Przemysław Sałek <psalek1999@gmail.com>
2023-01-22 16:02:02 +0100

    todo in footer

--- web-project/frontend/src/containers/pages/elements/Footer.js
+++ web-project/frontend/src/containers/pages/elements/Footer.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./Footer.css";
 
 class Footer extends Component {
   render() {
-    return <div className="footer">2023 © Web Project</div>;
+    return (
+      <div className="footer">
+        2023 © Web Project{" "}
+        <div
+          className="footer-test-app"
+          onClick={() => this.props.navigation("/todo")}
+        >
+          Test application
+        </div>
+      </div>
+    );
   }
 }
 
-export default Footer;
+export default function (props) {
+  const navigation = useNavigate();
+
+  return <Footer {...props} navigation={navigation} />;
+}
